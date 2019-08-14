df89da611266e4d8d8b787151aee5d6f77821c88
jb31 <jerembardon@gmail.com>
2019-08-14 19:20:20 +0200

    End project text

--- assets/js/app.js
+++ assets/js/app.js
@@ -58,10 +58,12 @@ import { TweenMax, TimelineMax } from 'gsap';
                 let skillsPrecisionContent = e.target.parentElement.firstElementChild.nextElementSibling;
 
                 if(skillItemContainer.classList.contains('active')) {
+                    e.target.classList.remove('active');
                     skillItemContainer.classList.remove('active');
                     tweenMaxAnimationSkills(skillsPresentationContent.children, true);
                     tweenMaxAnimationSkills(skillsPrecisionContent.children, false);
                 } else if (!skillItemContainer.classList.contains('active')) {
+                    e.target.classList.add('active');
                     skillItemContainer.classList.add('active');
                     tweenMaxAnimationSkills(skillsPresentationContent.children, false);
                     tweenMaxAnimationSkills(skillsPrecisionContent.children, true);
@@ -79,41 +81,55 @@ import { TweenMax, TimelineMax } from 'gsap';
     // Init slider
     let projectSlider = new Glide('.carousel__container', {
         type: 'carousel',
-        startAt: 1,
-        perView: 3,
+        startAt: 0,
+        perView: 2,
         focusAt: 'center',
-        gap: 24,
+        gap: 64,
+        peek: 90,
         breakpoints: {
             980: { 
-                perView: 1 ,
-                peek: 48
+                perView: 1,
+                peek: 32,
+                gap: 16
             }
         }
     });
     projectSlider.mount();
 
 
-    let sliderButtons = document.querySelectorAll('.glide__slide__button-fullscreen')
+
+    // Slider animation reveal project
+    let sliderButtons = document.querySelectorAll('.glide__slide__button-fullscreen');
+   
     sliderButtons.forEach(item => {
         item.addEventListener('click', function(e) {
-            let project = e.target.parentElement.parentElement.getAttribute('data-name-project')
-            let container = document.getElementById(project);
+            e.preventDefault();
+
+            let overflowWindow = document.documentElement;
+            overflowWindow.style.overflow = 'hidden'
+            // Enter animation for project
+            let project = e.target.parentElement.parentElement.getAttribute('data-name-project');
+            let containerWrapper = document.getElementById(project);
 
             let containerTimelineEnter = new TimelineMax({});
-            containerTimelineEnter.to(container, 0, {zIndex: 999}, 0.05).to(container, .4, {
-                height: '100vh', 
+            containerTimelineEnter.to(containerWrapper, 0, {
+                zIndex: 999
+            }, 0.05).to(containerWrapper, .4, { 
                 opacity: 1, 
              }, 0.05);
 
-            let containerButton = container.firstElementChild.lastElementChild; 
-            containerButton.addEventListener('click', function(e) {
-                let container = e.target.parentElement.parentElement;
 
+            // Project container close button
+            let containerCloseButton = containerWrapper.lastElementChild.firstElementChild;
+            containerCloseButton.addEventListener('click', function(e) {
+                overflowWindow.style.overflow = ''
+                let container = e.target.parentElement.parentElement;
                 let containerTimelineOut = new TimelineMax({});
                 containerTimelineOut.to(container, .4, {
-                    height: 0, 
                     opacity: 0, 
-                }, 0.05).to(container, .01, {zIndex: -1});
+                }, .2).to(container, .01, {
+                    zIndex: -1
+                })
             })
         })
     }) 
